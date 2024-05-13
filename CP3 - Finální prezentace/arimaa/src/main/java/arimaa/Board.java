cffaf87b90236b587560322c3976e85197bce737
Jaroslav Špác <spacjar@gmail.com>
2024-05-13 17:16:45 +0200

    feat: exception handling

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Board.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Board.java
@@ -1,56 +1,74 @@
 package arimaa;
 
-import arimaa.enums.PieceColor;
-
 public class Board {
+    private Piece[][] board;
 	private static final int ROW_SIZE = 8;
 	private static final int COL_SIZE = 8;
-  	private Piece[][] board;
 
 	public Board() {
         board = new Piece[ROW_SIZE][COL_SIZE];
 	}
 
+    // Basic methods for movement
     public Piece getPieceAt(int row, int col) {
+        if(row <= 0 && col <= 0 && row > ROW_SIZE && col > COL_SIZE) {
+            throw new IndexOutOfBoundsException("The selected row or column exceeds board size.");
+        }
         return board[row][col];
     }
 
-    public void placePiece(Piece piece, int row, int col) {
+    public void setPiece(Piece piece, int row, int col) {
+        if(row <= 0 && col <= 0 && row > ROW_SIZE && col > COL_SIZE) {
+            throw new IndexOutOfBoundsException("The selected row or column exceeds board size.");
+        }
         board[row][col] = piece;
     }
 
     public void removePiece(int row, int col) {
+        if(row <= 0 && col <= 0 && row > ROW_SIZE && col > COL_SIZE) {
+            throw new IndexOutOfBoundsException("The selected row or column exceeds board size.");
+        }
         board[row][col] = null;
     }
 
+    // Validation
     public boolean isOccupied(int row, int col) {
-        if(board[row][col] != null) {
+        if(getPieceAt(row, col) != null) {
             return true;
         }
         return false;
     }
 
     public boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol) {
-        // TODO: Validation logic
-        return false;
+        // TD: More logic
+        return isOccupied(toRow, toCol);
     }
 
-    public boolean movePiece(int fromRow, int fromCol, int toRow, int toCol) {
+
+    // IS IN TRAP
+    // IS FROZEN
+    // DOES IT HAVE ADJACENT PIECES
+
+
+    // Conditional method for movement
+    public void movePiece(int fromRow, int fromCol, int toRow, int toCol) {
+        if (!isOccupied(fromRow, fromCol)) {
+            throw new IllegalArgumentException("There is no piece at the specified location.");
+        }
+
         Piece piece = getPieceAt(fromRow, fromCol);
         removePiece(fromRow, fromCol);
-        placePiece(piece, toRow, toCol);
-        return true;
+
+        if (isValidMove(fromRow, fromCol, toRow, toCol)) {
+            setPiece(piece, toRow, toCol);
+        } else {
+            throw new IllegalArgumentException("Invalid move");
+        }
     }
 
-    public boolean isWin(PieceColor color) {
-        // TODO: Validation logic
-        return false;
-    }
 
-    public boolean isLoss(PieceColor color) {
-        // TODO: Validation logic
-        return false;
-    }
+    // CHECK IF THE GAME ENDED (WIN OR LOSE)
+
 
 	public void printBoard() {
 		System.out.println("-   A    B    C    D    E    F    G    H");
