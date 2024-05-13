51511ee489a410e21fabeda38c5dfff8207db6cf
Jaroslav Špác <spacjar@gmail.com>
2024-05-13 19:18:22 +0200

    feat: advanced checks - trap, frozen, adjacent pieces

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Board.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Board.java
@@ -1,5 +1,10 @@
 package arimaa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import arimaa.enums.PieceState;
+
 public class Board {
     private Piece[][] board;
 	private static final int ROW_SIZE = 8;
@@ -40,15 +45,68 @@ public class Board {
     }
 
     public boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol) {
-        // TD: More logic
-        return isOccupied(toRow, toCol);
+        // ! TD: More logic (move max 4 spaces);
+        return !isOccupied(toRow, toCol);
     }
 
 
-    // IS IN TRAP
-    // IS FROZEN
-    // DOES IT HAVE ADJACENT PIECES
+    // Get all adjacent pieces to the piece
+    public List<Piece> getAdjacentPieces(int row, int col) {
+        List<Piece> adjacentPieces = new ArrayList<>();
+        
+        // ! TD: Shitty code - Refactor logic!!
+        if(row+1 > 0 && col > 0 && row+1 <= ROW_SIZE && col <= COL_SIZE) {
+            adjacentPieces.add(getPieceAt(row+1, col));
+        } else if(row-1 > 0 && col > 0 && row-1 <= ROW_SIZE && col <= COL_SIZE) {
+            adjacentPieces.add(getPieceAt(row-1, col));
+        } else if(row > 0 && col+1 > 0 && row <= ROW_SIZE && col+1 <= COL_SIZE) {
+            adjacentPieces.add(getPieceAt(row, col+1));
+        } else if(row > 0 && col-1 > 0 && row <= ROW_SIZE && col-1 <= COL_SIZE) {
+            adjacentPieces.add(getPieceAt(row, col-1));
+        }
+    
+        return adjacentPieces;
+    }
 
+    // Checks if the piece has friendly adjacent pieces
+    public boolean hasAdjacentFriendlyPieces(int row, int col) {
+        Piece currentPiece = getPieceAt(row, col);
+        List<Piece> adjacentPieces = getAdjacentPieces(row, col);
+    
+        for (Piece piece : adjacentPieces) {
+            if (piece != null && piece.getColor() == currentPiece.getColor()) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+    // Checks if the piece is frozen
+    public boolean isFrozen(int row, int col) {
+        Piece currentPiece = getPieceAt(row, col);
+        List<Piece> adjacentPieces = getAdjacentPieces(row, col);
+
+        for (Piece piece : adjacentPieces) {
+            // ! TD: Add type to number (weight)
+            // if (piece != null && piece.getColor() != currentPiece.getColor() && piece.getType() != currentPiece.getType()) {
+            if (piece != null && piece.getColor() != currentPiece.getColor()) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+
+    // Checks if the piece is in the trap field
+    public boolean isInTrap(int row, int col) {
+        // ? note(not important): Think of a way to make it dynamic. For example if I set the board to 16x16, than these sqaures will not be sufficient. However, due to the rules, this cannot happen.
+        if ((row == 2 && col == 2) || (row == 2 && col == 5) || (row == 5 && col == 2) || (row == 5 && col == 5)) { 
+            return true;
+        }
+        return false;
+    }
 
     // Conditional method for movement
     public void movePiece(int fromRow, int fromCol, int toRow, int toCol) {
@@ -56,18 +114,35 @@ public class Board {
             throw new IllegalArgumentException("There is no piece at the specified location.");
         }
 
+        if (isFrozen(fromCol, fromRow)) {
+            throw new IllegalArgumentException("This piece is frozen");
+        }
+
         Piece piece = getPieceAt(fromRow, fromCol);
         removePiece(fromRow, fromCol);
 
-        if (isValidMove(fromRow, fromCol, toRow, toCol)) {
+        // The !isFrozen() does not need to be here, since I check it above (but i am going to think about it)
+        if (isValidMove(fromRow, fromCol, toRow, toCol) && !isFrozen(fromCol, fromRow)) {
             setPiece(piece, toRow, toCol);
         } else {
             throw new IllegalArgumentException("Invalid move");
         }
+
+        if(isInTrap(toRow, toCol) && !hasAdjacentFriendlyPieces(toRow, toCol)) {
+            piece.setState(PieceState.DEAD);
+            removePiece(toRow, toCol);
+        }
+
+        if(isFrozen(toCol, toRow)) {
+            piece.setState(PieceState.FROZEN);
+        }
     }
 
+    // TD: PUSH ENEMY PIECE
 
-    // CHECK IF THE GAME ENDED (WIN OR LOSE)
+    // TD: PULL ENEMY PIECE
+
+    // TD: CHECK IF THE GAME ENDED (WIN OR LOSE)
 
 
 	public void printBoard() {
@@ -84,8 +159,8 @@ public class Board {
 					System.out.print(" " + piece.toString() + " ");
 				}
 			}
-
-			System.out.println();
+            
+            System.out.println();
         }
     }
 }
