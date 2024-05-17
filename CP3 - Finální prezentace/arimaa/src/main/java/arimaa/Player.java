a91fed99f81acd52a070c8c80b4c33d61e2ba905
Jaroslav Špác <spacjar@gmail.com>
2024-05-17 18:39:39 +0200

    feat: basic player properties

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Player.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Player.java
@@ -1,5 +1,15 @@
 package arimaa;
 
+import arimaa.enums.PieceColor;
+
 public class Player {
-    
+    private PieceColor color;
+
+    public Player(PieceColor color) {
+        this.color = color;
+    }
+
+    public PieceColor getColor() {
+        return color;
+    }
 }
