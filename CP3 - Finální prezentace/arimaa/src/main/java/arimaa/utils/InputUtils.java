985aed6deda79d436c3e012ca3624ace0c986345
Jaroslav Špác <spacjar@gmail.com>
2024-05-19 20:43:55 +0200

    refactor: input utils

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/utils/InputUtils.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/utils/InputUtils.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class InputUtils {
 
+    /**
+     * Prompts the user with a message and retrieves their input from the console.
+     *
+     * @param message the message to display to the user
+     * @return the user's input as a String
+     */
     public static String getUserInput(String message) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(message);
@@ -11,38 +17,47 @@ public class InputUtils {
         return userInput;
     }
 
-    public static Number stringToInt(String str) {
-        try {
-            return Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid number format.");
-            return null;
-        }
-    }
 
+    /**
+     * Reads an integer value from a user input.
+     * If the user input cannot be converted to an Integer, an error message is printed and the user is prompted again.
+     *
+     * @param message the message to display to the user
+     * @return converted integer value entered by the user
+     */
     public static int getIntFromInput(String message) {
-        Integer number = null;
-        while (number == null) {
-            String userInput = getUserInput(message);
-            try {
-                number = Integer.parseInt(userInput);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid number.");
-            }
-        }
-        return number;
-    }
-
-    public static Integer getPositionsFromInput(String message) {
         while (true) {
             String userInput = getUserInput(message);
+
+            try {
+                return NumberUtils.stringToInteger(userInput);
+            } catch (NumberFormatException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+
+    /**
+     * Handles the user input for a position and returns it as an Integer.
+     * If the user inputs "skip", null is returned.
+     * If the user input cannot be converted to an Integer, an error message is printed and the user is prompted again.
+     *
+     * @param message the message to display to the user
+     * @return the user input as an Integer, or null if the user inputs "skip"
+     */
+    public static Integer handlePositionInput(String message) {
+        while (true) {
+            String userInput = getUserInput(message);
+
             if (userInput.equalsIgnoreCase("skip")) {
                 return null;
             }
+
             try {
-                return Integer.parseInt(userInput);
+                return NumberUtils.stringToInteger(userInput);
             } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid number or 'skip'.");
+                System.err.println(e.getMessage());
             }
         }
     }
