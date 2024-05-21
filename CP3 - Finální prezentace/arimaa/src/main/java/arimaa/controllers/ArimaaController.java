e2098970724af6afa919c1d45226b5e7cc251ec6
Jaroslav Špác <spacjar@gmail.com>
2024-05-22 01:49:18 +0200

    feat: input for row and col

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/controllers/ArimaaController.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/controllers/ArimaaController.java
@@ -3,14 +3,28 @@ package arimaa.controllers;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ArimaaController {
     @FXML
     private Label feedbackMessage;
 
     @FXML
+    private TextField rowInput;
+
+    @FXML
+    private TextField colInput;
+
+    @FXML
     private void submit() throws IOException {
         System.out.println("Submit");
-        feedbackMessage.setText("It works!");
-    }    
+
+        String rowInputText = rowInput.getText();
+        String colInputText = colInput.getText();
+
+        System.out.println("Row: " + rowInputText);
+        System.out.println("Col: " + colInputText);
+
+        feedbackMessage.setText("Submited");
+    }
 }
