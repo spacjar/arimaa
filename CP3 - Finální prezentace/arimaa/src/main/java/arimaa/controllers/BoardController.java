1cb609f1f1952dc4b10faa3422c6f4d54c64d035
Jaroslav Špác <spacjar@gmail.com>
2024-05-22 02:02:26 +0200

    feat: basic board generation

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/controllers/BoardController.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/controllers/BoardController.java
@@ -1,8 +1,25 @@
 package arimaa.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class BoardController {
     @FXML
-    private Label feedbackMessage;
+    private GridPane boardGrid;
+
+    public void initialize() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Pane square = new Pane();
+                square.setPrefSize(60, 60);
+                if ((row + col) % 2 == 0) {
+                    square.setStyle("-fx-background-color: white;");
+                } else {
+                    square.setStyle("-fx-background-color: gray;");
+                }
+                boardGrid.add(square, col, row);
+            }
+        }
+    }
 }
