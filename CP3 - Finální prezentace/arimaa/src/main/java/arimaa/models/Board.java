60bf76192f4750cc9012acac76f05277d750ae4d
Jaroslav Špác <spacjar@gmail.com>
2024-05-26 01:42:26 +0200

    feat: pushing logic + logger cleanup

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/models/Board.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/models/Board.java
@@ -96,12 +96,12 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isOccupied(int row, int col) throws IndexOutOfBoundsException {
-        logger.info("Checking if position at row " + row + ", column " + col + " is occupied.");
+        // logger.info("Checking if position at row " + row + ", column " + col + " is occupied.");
         if(getPieceAt(row, col) == null) {
-            logger.info("Position is not occupied.");
+            // logger.info("Position is not occupied.");
             return false;
         }
-        logger.info("Position is occupied.");
+        // logger.info("Position is occupied.");
         return true;
     }
 
@@ -114,13 +114,13 @@ public class Board {
      * @return true if the piece is in a trap field, false if not
      */
     public boolean isInTrap(int row, int col) {
-        logger.info("Checking if position at row " + row + ", column " + col + " is a trap.");
+        // logger.info("Checking if position at row " + row + ", column " + col + " is a trap.");
         // ? TD: Think of a way to make it dynamic. For example if I set the board to 16x16, than these sqaures will not be sufficient. However, due to the rules, this cannot happen.
         if ((row == 2 && col == 2) || (row == 2 && col == 5) || (row == 5 && col == 2) || (row == 5 && col == 5)) { 
-            logger.info("Position is a trap.");
+            // logger.info("Position is a trap.");
             return true;
         }
-        logger.info("Position is not a trap.");
+        // logger.info("Position is not a trap.");
         return false;
     }
 
@@ -137,13 +137,13 @@ public class Board {
     public boolean isOneStep(int fromRow, int fromCol, int toRow, int toCol) {
         // ? TD: In the furute, the code could calculate the distance the player will take - public int getPathCost(int fromRow, int fromCol, int toRow, int toCol)
 
-        logger.info("Checking if move from row " + fromRow + ", column " + fromCol + " to row " + toRow + ", column " + toCol + " is one step.");
+        // logger.info("Checking if move from row " + fromRow + ", column " + fromCol + " to row " + toRow + ", column " + toCol + " is one step.");
         if (fromRow == toRow && Math.abs(fromCol - toCol) == 1) {
-            logger.info("Move is one step.");
+            // logger.info("Move is one step.");
             return true;
         }
         if (fromCol == toCol && Math.abs(fromRow - toRow) == 1) {
-            logger.info("Move is one step.");
+            // logger.info("Move is one step.");
             return true;
         }   
     
@@ -164,7 +164,7 @@ public class Board {
      */
     // ! TD: Cleanup this method logic, the goal is to have the method return false by default, and true if it is really true!
     public boolean isRabbitMoveValid(int fromRow, int fromCol, int toRow, int toCol) throws IndexOutOfBoundsException {
-        logger.info("Checking if rabbit move from row " + fromRow + ", column " + fromCol + " to row " + toRow + ", column " + toCol + " is valid.");
+        // logger.info("Checking if rabbit move from row " + fromRow + ", column " + fromCol + " to row " + toRow + ", column " + toCol + " is valid.");
 
         Piece piece = getPieceAt(fromRow, fromCol);
         // ? TD: Fix when a player can choose sides, now it is that the GOLDEN player always has to start at the [0] index of the array, and the SILVER player at the [7] index of the array
@@ -179,7 +179,7 @@ public class Board {
             }
         }
 
-        logger.info("Move is one step.");
+        // logger.info("Move is one step.");
         return true;
     }
 
@@ -193,7 +193,7 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public List<int[]> getAdjacentPiecePositions(int row, int col) throws IndexOutOfBoundsException {
-        logger.info("Getting adjacent piece positions for row " + row + ", column " + col + ".");
+        // logger.info("Getting adjacent piece positions for row " + row + ", column " + col + ".");
         List<int[]> adjacentPieces = new ArrayList<int[]>();
 
         if(row < 0 || col < 0 || row >= ROW_SIZE || col >= COL_SIZE) {
@@ -218,7 +218,7 @@ public class Board {
             adjacentPieces.add(new int[]{row, col-1});
         }
     
-        logger.info("Found " + adjacentPieces.size() + " adjacent pieces.");
+        // logger.info("Found " + adjacentPieces.size() + " adjacent pieces.");
         return adjacentPieces;
     }
 
@@ -232,7 +232,7 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean hasAdjacentFriendlyPieces(int row, int col) throws IndexOutOfBoundsException {
-        logger.info("Checking if there are adjacent friendly pieces at row " + row + ", column " + col + ".");
+        // logger.info("Checking if there are adjacent friendly pieces at row " + row + ", column " + col + ".");
         Piece currentPiece = getPieceAt(row, col);
         List<int[]> adjacentPiecePositions = getAdjacentPiecePositions(row, col);
     
@@ -240,12 +240,30 @@ public class Board {
             Piece piece = getPieceAt(position[0], position[1]);
             // ? TD: Think if I need the piece != null, since I check it in the getAdjacentPiecePositions() method.
             if (piece != null && currentPiece != null && piece.getColor() == currentPiece.getColor()) {
-                logger.info("Found adjacent friendly piece.");
+                // logger.info("Found adjacent friendly piece.");
                 return true;
             }
         }
 
         logger.info("No adjacent friendly pieces found.");
+        return false;
+    }
+
+
+    public boolean hasAdjacentEnemyPiecesWithHigherValue(int row, int col) {
+        Piece currentPiece = getPieceAt(row, col);
+        List<int[]> adjacentPiecePositions = getAdjacentPiecePositions(row, col);
+
+        for (int[] position : adjacentPiecePositions) {
+            Piece enemyPiece = getPieceAt(position[0], position[1]);
+            // ? TD: Think if I need the piece != null, since I check it in the getAdjacentPiecePositions() method.
+            if (currentPiece != null && enemyPiece != null && currentPiece.getColor() != enemyPiece.getColor() && currentPiece.getPieceWeight() < enemyPiece.getPieceWeight()) {
+                // logger.info("Found adjacent friendly piece.");
+                return true;
+            }
+        }
+
+        logger.info("There are no enemy pieces with a higher piece weight");
         return false;
     }
 
@@ -259,7 +277,7 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isFrozen(int row, int col) throws IndexOutOfBoundsException {
-        logger.info("Checking if piece at row " + row + ", column " + col + " is frozen.");
+        // logger.info("Checking if piece at row " + row + ", column " + col + " is frozen.");
         Piece currentPiece = getPieceAt(row, col);
         List<int[]> adjacentPiecePositions = getAdjacentPiecePositions(row, col);
 
@@ -272,7 +290,7 @@ public class Board {
             }
         }
     
-        logger.info("Piece is not frozen.");
+        // logger.info("Piece is not frozen.");
         return false;
     }
 
@@ -287,7 +305,7 @@ public class Board {
      * @throws IllegalArgumentException if the piece doesn't exist at the specified location, if the piece is frozen, if the move is invalid (not one-step or rabbit moving backwards), or if there is already a piece at the new position
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
-    public void movePiece(int fromRow, int fromCol, int toRow, int toCol) throws IllegalArgumentException, IndexOutOfBoundsException {
+    public void movePiece(int fromRow, int fromCol, int toRow, int toCol, boolean isPushing, boolean isPulling) throws IllegalArgumentException, IndexOutOfBoundsException {
         logger.info("Attempting to move piece from row " + fromRow + ", column " + fromCol + " to row " + toRow + ", column " + toCol + ".");
 
         // Check if the piece exists
@@ -297,7 +315,7 @@ public class Board {
         }
 
         // Check if the piece is frozen
-        if (isFrozen(fromRow, fromCol)) {
+        if (isFrozen(fromRow, fromCol) && isPushing != true) {
             logger.warning("Piece is frozen.");
             throw new IllegalArgumentException("This piece is frozen!");
         }
@@ -309,7 +327,7 @@ public class Board {
         }
 
         // Check if the peice is a rabbit trying to move backwards
-        if(!isRabbitMoveValid(fromRow, fromCol, toRow, toCol)) {
+        if(!isRabbitMoveValid(fromRow, fromCol, toRow, toCol) && isPushing != true) {
             logger.warning("Invalid move, rabbits cannot move backwards.");
             throw new IllegalArgumentException("This move is invalid, a rabbit piece cannot move backwards!");
         }
@@ -334,7 +352,7 @@ public class Board {
             int positionFromCol = position[1];
 
             if(isInTrap(positionFromRow, positionFromCol) && !hasAdjacentFriendlyPieces(positionFromRow, positionFromCol)) {
-                logger.info("Adjacent piece at previous position is in trap and not protected. Removing piece.");
+                // logger.info("Adjacent piece at previous position is in trap and not protected. Removing piece.");
                 removePiece(positionFromRow, positionFromCol);
                 return;
             }
@@ -342,7 +360,7 @@ public class Board {
 
         // Check after moving the piece, if the piece is in a trap
         if(isInTrap(toRow, toCol) && !hasAdjacentFriendlyPieces(toRow, toCol)) {
-            logger.info("Moved piece is in trap and not protected. Removing piece.");
+            // logger.info("Moved piece is in trap and not protected. Removing piece.");
             removePiece(toRow, toCol);
             return;
         }
@@ -355,7 +373,7 @@ public class Board {
             int positionToCol = position[1];
 
             if(isInTrap(positionToRow, positionToCol) && !hasAdjacentFriendlyPieces(positionToRow, positionToCol)) {
-                logger.info("Adjacent piece at new position is in trap and not protected. Removing piece.");
+                // logger.info("Adjacent piece at new position is in trap and not protected. Removing piece.");
                 removePiece(positionToRow, positionToCol);
                 return;
             }
@@ -374,13 +392,13 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isGameWon(Player currentPlayer) throws IndexOutOfBoundsException {
-        logger.info("Checking if game is won by current player: " + currentPlayer.getColor());
+        // logger.info("Checking if game is won by current player: " + currentPlayer.getColor());
         if(isRabbitOnTheOtherSide(currentPlayer)) {
             logger.info("Game is won by current player.");
             return true;
         }
 
-        logger.info("Game is not won by current player.");
+        // logger.info("Game is not won by current player.");
         return false;
     }
 
@@ -393,13 +411,13 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isGameLost(Player currentPlayer) throws IndexOutOfBoundsException {
-        logger.info("Checking if game is lost by current player: " + currentPlayer.getColor());
+        // logger.info("Checking if game is lost by current player: " + currentPlayer.getColor());
         if(isEveryPieceFrozen(currentPlayer) || isPlayerUnableToMove(currentPlayer) || isPlayerWithoutAllRabbits(currentPlayer)) {
             logger.info("Game is lost by current player.");
             return true;
         }
 
-        logger.info("Game is not lost by current player.");
+        // logger.info("Game is not lost by current player.");
         return false;
     }
 
@@ -412,7 +430,7 @@ public class Board {
      * @throws IndexOutOfBoundsException if the row or column index is out of bounds
      */
     public boolean isRabbitOnTheOtherSide(Player player) throws IndexOutOfBoundsException {
-        logger.info("Checking if there is a rabbit piece on the other side for player: " + player.getColor());
+        // logger.info("Checking if there is a rabbit piece on the other side for player: " + player.getColor());
         PieceColor playerColor = player.getColor();
 
         int targetRow = playerColor == PieceColor.GOLDEN ? 7 : 0;
@@ -420,7 +438,7 @@ public class Board {
         for (int col = 0; col < COL_SIZE; col++) {
             Piece piece = getPieceAt(targetRow, col);
             if (piece != null && piece.getType() == PieceType.RABBIT && piece.getColor() == playerColor) {
-                logger.info("Found rabbit piece on the other side for player.");
+                // logger.info("Found rabbit piece on the other side for player.");
                 return true;
             }
         }
@@ -438,12 +456,12 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isEveryPieceFrozen(Player player) throws IndexOutOfBoundsException {
-        logger.info("Checking if every piece belonging to the current player is frozen.");
+        // logger.info("Checking if every piece belonging to the current player is frozen.");
         for (int row = 0; row < ROW_SIZE; row++) {
             for (int col = 0; col < COL_SIZE; col++) {
                 Piece piece = getPieceAt(row, col);
                 if (piece != null && piece.getColor() == player.getColor() && !isFrozen(row, col)) {
-                    logger.info("Found a piece that is not frozen.");
+                    // logger.info("Found a piece that is not frozen.");
                     return false;
                 }
             }
@@ -463,7 +481,7 @@ public class Board {
      * @throws IndexOutOfBoundsException if the specified row or column exceeds the board size
      */
     public boolean isPlayerUnableToMove(Player player) throws IndexOutOfBoundsException {
-        logger.info("Checking if the player is unable to make any moves.");
+        // logger.info("Checking if the player is unable to make any moves.");
         for (int row = 0; row < ROW_SIZE; row++) {
             for (int col = 0; col < COL_SIZE; col++) {
                 Piece piece = getPieceAt(row, col);
@@ -471,7 +489,7 @@ public class Board {
 
                 // Check if the current piece is a) not frozen and b) has less than 4 pieces adjacent to it - this way you can determine if the piece is able to move
                 if (piece != null && piece.getColor() == player.getColor() && !isFrozen(row, col) && numOfAdjacentPieces < 4) {
-                    logger.info("Found a piece that can move.");
+                    // logger.info("Found a piece that can move.");
                     return false;
                 }
             }
@@ -490,14 +508,14 @@ public class Board {
      * @throws IndexOutOfBoundsException if the row or column index is out of bounds
      */
     public boolean isPlayerWithoutAllRabbits(Player player) throws IndexOutOfBoundsException {
-        logger.info("Checking if the player: " + player.getColor() + " has no rabbits left on the board.");
+        // logger.info("Checking if the player: " + player.getColor() + " has no rabbits left on the board.");
         PieceColor playerColor = player.getColor();
 
         for (int row = 0; row < ROW_SIZE; row++) {
             for (int col = 0; col < COL_SIZE; col++) {
                 Piece piece = getPieceAt(row, col);
                 if (piece != null && piece.getType() == PieceType.RABBIT && piece.getColor() == playerColor) {
-                    logger.info("Found a rabbit belonging to the player.");
+                    // logger.info("Found a rabbit belonging to the player.");
                     return false;
                 }
             }
