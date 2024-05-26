79ae35024fede55f330fb486b601a587e4505b90
Jaroslav Špác <spacjar@gmail.com>
2024-05-26 20:41:33 +0200

    feat: displaying multiple views in one scene

--- CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Main.java
+++ CP3 - Finální prezentace/arimaa/src/main/java/arimaa/Main.java
@@ -2,8 +2,8 @@ package arimaa;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import java.util.logging.Logger;
@@ -42,38 +42,43 @@ public class Main extends Application {
         boardController.setArimaaController(arimaaController);
 
         // Game state
-        boolean isGameStarted = false;
-        boolean isGameRunning = false;
+        boolean isGameRunning = true;
+        boolean isGameSetup = false;
+        boolean isGameEnd = false;
     
-        Pair<Node, Object> content;
+        VBox rootContainer;
         try {
-            content = loadFXML(isGameStarted, isGameRunning);
+            if (!isGameRunning && !isGameSetup && !isGameEnd) {
+                Pair<Node, Object> content = loadFXML("./views/StartView.fxml", dummyController);
+                rootContainer = new VBox(content.getKey());
+            } else if (isGameRunning && !isGameSetup && !isGameEnd) {
+                Pair<Node, Object> content1 = loadFXML("./views/StartView.fxml", dummyController);
+                Pair<Node, Object> content2 = loadFXML("./views/StartView.fxml", dummyController);
+                // Pair<Node, Object> content1 = loadFXML("./views/BoardView.fxml", boardController);
+                // Pair<Node, Object> content2 = loadFXML("./views/ArimaaSetupView.fxml", arimaaController);
+                rootContainer = new VBox(content1.getKey(), content2.getKey());  
+            } else if (isGameRunning && isGameSetup && !isGameEnd) {
+                Pair<Node, Object> content1 = loadFXML("./views/StartView.fxml", dummyController);
+                Pair<Node, Object> content2 = loadFXML("./views/StartView.fxml", dummyController);
+                // Pair<Node, Object> content1 = loadFXML("./views/BoardView.fxml", boardController);
+                // Pair<Node, Object> content2 = loadFXML("./views/ArimaaView.fxml", arimaaController);
+                rootContainer = new VBox(content1.getKey(), content2.getKey());
+            } else {
+                Pair<Node, Object> content = loadFXML("./views/EndView.fxml", dummyController);
+                rootContainer = new VBox(content.getKey());
+            }
         } catch (IOException e) {
             logger.severe("Error loading FXML file: " + e.getMessage());
             return;
         }
     
-        Scene scene = new Scene((Parent) content.getKey());
-        // Object controller = content.getValue();
-        // Do something with the controller...
-    
+        Scene scene = new Scene(rootContainer);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private Pair<Node, Object> loadFXML(boolean isGameStarted, boolean isGameRunning) throws IOException {
-        FXMLLoader loader;
-        Object controller;
-        if (!isGameStarted) {
-            loader = new FXMLLoader(getClass().getResource("./views/StartView.fxml"));
-            controller = dummyController;
-        } else if (isGameRunning) {
-            loader = new FXMLLoader(getClass().getResource("./views/GameView.fxml"));
-            controller = dummyController;
-        } else {
-            loader = new FXMLLoader(getClass().getResource("./views/EndView.fxml"));
-            controller = dummyController;
-        }
+    private Pair<Node, Object> loadFXML(String fxmlPath, Object controller) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
         loader.setControllerFactory(c -> controller);
         Node root = (Node) loader.load();
         return new Pair<>(root, controller);
@@ -90,80 +95,4 @@ public class Main extends Application {
         // Launch the application
         launch(args);
     }
-
-
-
-
-
-    // private static final Logger logger = Logger.getLogger(Main.class.getName());
-
-    // @Override
-    // public void start(Stage primaryStage) {
-    //     try {
-    //         logger.info("Starting application.");
-
-    //         // Create the root pane for the application
-    //         BorderPane root = new BorderPane();
-            
-    //         // Create the game board and the game instance
-    //         logger.info("Creating board.");
-    //         Board board = new Board();
-    //         Arimaa arimaa = new Arimaa();
-            
-    //         // Load the board view, set the board, and initialize the controller
-    //         logger.info("Loading BoardView.fxml.");
-    //         FXMLLoader boardLoader = new FXMLLoader(getClass().getResource("./views/BoardView.fxml"));
-    //         Parent boardView = boardLoader.load();
-    //         root.setCenter(boardView);
-    //         BoardController boardController = boardLoader.getController();
-    //         logger.info("Setting board in BoardController.");
-    //         boardController.setBoard(board);
-    //         logger.info("Initializing BoardController.");
-    //         boardController.initialize();
-            
-    //         // Load the game view, set the board, game, and other necessary properties, and initialize the controller
-    //         logger.info("Loading ArimaaView.fxml.");
-    //         FXMLLoader arimaaLoader = new FXMLLoader(getClass().getResource("./views/ArimaaView.fxml"));
-    //         Parent arimaaView = arimaaLoader.load();
-    //         root.setBottom(arimaaView);
-    //         ArimaaController arimaaController = arimaaLoader.getController();
-    //         logger.info("Setting properties in ArimaaController.");
-    //         arimaaController.setBoard(board);
-    //         arimaaController.setBoardController(boardController);
-    //         arimaaController.setArimaa(arimaa);
-    //         arimaaController.setRoot(root); // Pass the root to the controller
-    //         arimaaController.setStage(primaryStage); // Pass the stage to the controller
-    //         logger.info("Initializing ArimaaController.");
-    //         arimaaController.initialize();
-
-    //         // Set the ArimaaController in the BoardController
-    //         logger.info("Setting ArimaaController in BoardController.");
-    //         boardController.setArimaaController(arimaaController);
-
-    //         // Set the board in the Arimaa game instance
-    //         logger.info("Setting board to the Arimaa game instance.");
-    //         arimaa.setBoard(board);
-
-    //         // Set up the scene and stage and show the stage
-    //         logger.info("Setting up scene and stage.");
-    //         Scene scene = new Scene(root, 960, 960);
-    //         primaryStage.setScene(scene);
-    //         primaryStage.setTitle("Arimaa Game");
-    //         primaryStage.show();
-    //     } catch (Exception e) {
-    //         logger.severe("(!) ERROR: " + e.getMessage());
-    //     }
-    // }
-
-    // public static void main(String[] args) {
-    //     // Set the log level based on a startup parameter
-    //     if (args.length > 0 && args[0].equals("debug")) {
-    //         logger.setLevel(Level.ALL);
-    //     } else {
-    //         logger.setLevel(Level.OFF);
-    //     }
-
-    //     // Launch the application
-    //     launch(args);
-    // }
 }
